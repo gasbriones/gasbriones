439dd36d1beb0c8e55374d92632188df8ba08099
Gaston Briones <mauricio_briones@artear.com.ar>
2015-06-15 18:19:06 -0300

    fix

--- wp-content/themes/gasbriones/header-part.php
+++ wp-content/themes/gasbriones/header-part.php
@@ -1,13 +1,28 @@
 <header id="header" class="clearfix">
         <div class="wrapper clearfix">
             <div class="logo-wrapper">
-                <a class="logo" href="#">
-                    <img title="gasbriones diseño web" alt="gasbriones" src="http://livedemo00.template-help.com/woocommerce_53759/wp-content/themes/theme53759/images/logo.png">
+                <a class="logo" href="#" title="gas briones">
+                    <img title="gasbriones diseño web" alt="gasbriones" src="<?php echo get_template_directory_uri(); ?>/images/logo.png">
+                    <div class="left">
+                        <h1>
+                            <span class="logo-name">gb</span>
+                        </h1>
+                        <h2 class="logo-tag-line">WEB - DSÑ & DEV </h2>
+                    </div>
                 </a>
             </div>
             <div class="menu-wrapper">
                 <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header-menu' ) ); ?>
             </div>
-            <div class="social-wrapper"></div>
+            <div class="social-wrapper">
+                <ul class="social">
+                    <li>
+                        <a href="#" class="fb"><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" class="tw"><i class="fa fa-twitter"></i></a>
+                    </li>
+                </ul>
+            </div>
         </div>
 </header>
