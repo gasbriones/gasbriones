62b77c8b5cdb5bdc322404c723c03f65adcbf186
Gaston Briones <gbriones@cmd.com.ar>
2015-06-16 10:22:02 -0300

    fix

--- wp-content/themes/gasbriones/functions.php
+++ wp-content/themes/gasbriones/functions.php
@@ -3,3 +3,6 @@ function register_my_menu() {
     register_nav_menu('header-menu',__( 'Header Menu' ));
 }
 add_action( 'init', 'register_my_menu' );
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
